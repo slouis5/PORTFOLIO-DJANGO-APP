ad0d8ab62d46acbe0c747c81efc0128b6905194b
Serginau LOUIS <serginaulouis@gmail.com>
2024-03-31 06:53:33 -0400

    template pages home and experiences

--- portfolio/summary/urls.py
+++ portfolio/summary/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.index, name="index"),
+    path("experiences/", views.experience, name="experiences")
 
 ]
--- portfolio/summary/views.py
+++ portfolio/summary/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, HttpResponse
-from .models import Company, SkillCategory, Me, Service, Social
+from .models import Company, SkillCategory, Me, Service, Social, Experience
+
+
+
+def footer():
+    companies_logo = Company.objects.all()
+    services = Service.objects.all()
+    socials = Social.objects.all()
+    return dict({'companies': companies_logo, "services": services, "socials": socials})
 
 # Views
 def index(request):
-    companies_logo = Company.objects.all()
     skills_cat = SkillCategory.objects.all()
     gen_desc = Me.objects.all()[:1].get()
-    services = Service.objects.all()
-    socials = Social.objects.all()
-    return render(request, "summary/index.html", { 'companies': companies_logo, 'skills_categories': skills_cat, 'about': gen_desc,
-                                                  "services": services, "socials": socials})
+    actual_title = Experience.objects.filter(is_last=True)
+    return render(request, "summary/index.html", {'skills_categories': skills_cat, 'about': gen_desc,
+                                                  'current_title':actual_title} | footer())
 
+
+def experience(request):
+    experiences = Experience.objects.all().order_by('-period_start',)
+
+    return render(request, 'summary/experiences.html', {'experiences': experiences} | footer())
 
